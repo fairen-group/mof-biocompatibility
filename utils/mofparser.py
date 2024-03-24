f46a000fb08dc335ab420babba6dadfef09c9a2c
Dhruv Menon <128166706+dhruv-menon@users.noreply.github.com>
2024-03-24 19:08:25 +0000

    Update mofparser.py

--- utils/mofparser.py
+++ utils/mofparser.py
@@ -1,8 +1,8 @@
 '''
-In this python script, we need to parse the MOF cif file in a way that is appropriate to be passed to moffragmentor. We do so due to issues encountered
-in structures obtained from the CSD>
+In this python script, we need to parse the MOF cif file in a way that is appropriate to be passed to moffragmentor. We do so because in some cases, there are issues encountered
+in structures obtained from the CSD
 
-Thus, first the structures should be passed through this script, and then through the script for fragmentation. 
+Thus, in such cases, first the structures should be passed through this script, and then through the script for fragmentation. 
 Again, the code is written to be compatible with a high-throughput screen.
 
 A new directory of CIF files is saved post-parsing.
@@ -15,7 +15,7 @@ import os
 '''
 Below, add the path to the directory with the saved structures.
 '''
-directory = 'ZrMOF_clean/'
+directory = '/' # add the path to the directory
 files = os.listdir(directory)
 index = 0
 while index < len(files):
@@ -32,7 +32,7 @@ while index < len(files):
             '''
             Below, again specify the directory path where you want the parsed files to be saved.
             '''
-            s.to('ZrMOF_parsed/' + str(filename))
+            s.to('parsed/' + str(filename))
             print('Finished parsing ' + str(filename))
             index += 1
     except:
